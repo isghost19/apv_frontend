f75582cbb3cb4bc714891be34b16699871924474
IsGhost <jamaica.1999@hotmail.com>
2023-11-13 14:03:45 -0400

    fixed rute footer to Footer

--- src/layout/RutaProtegida.jsx
+++ src/layout/RutaProtegida.jsx
@@ -1,6 +1,6 @@
 import { Outlet, Navigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
-import { Footer } from "../components/footer";
+import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 
 
